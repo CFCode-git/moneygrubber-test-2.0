bf2b3bf4eb60aa464ec123bf545a73137fe0341d
CFCode-git <435184766@qq.com>
2020-04-03 15:23:38 +0800

    创建Icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
     |
-    <router-link to="/statistics">明细</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      明细
+    </router-link>
     |
-    <router-link to="/chart">图表</router-link>
+    <router-link to="/chart">
+      <Icon name="charts"/>
+      图表
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+  import x from '@/assets/icons/charts.svg';
+
+  console.log(x);
   export default {
     name: 'Nav'
   };
