810196696cc66441a816f6f2402d3f583defc53f
CFCode-git <435184766@qq.com>
2020-04-04 22:09:46 +0800

    使用 v-model 代替 value 和 input 绑定

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,15 +1,31 @@
 <template>
+  <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" placeholder="写点备注吧~">
+      <input type="text"
+             v-model="value"
+             placeholder="写点备注吧~">
+      <!--           :value="value"-->
+      <!--           @input="onInput"-->
     </label>
+  </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Notes'
-  };
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Notes extends Vue {
+    value = '';
+
+    onInput(event: KeyboardEvent) {
+      const input = event.target as HTMLInputElement;
+      this.value = input.value;
+    }
+  }
 </script>
+
 
 <style scoped lang="scss">
   .notes {
